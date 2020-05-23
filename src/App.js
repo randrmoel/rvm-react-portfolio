cf54242ffce5d4605f2d1b9393b7b839a038fc87
Robert Moel <robert.moel@rvmconsulting1.com>
2020-05-23 11:47:29 -0400

    Basic Tab Structure working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import './App.css';
+import About from './components/About'
+import Contact from './components/Contact'
+import Portfolio from './components/Portfolio'
+import {Nav, Container} from 'react-bootstrap'
+
 
 function App() {
+
+  // useEffect(()=>{
+
+  // },
+  // [/* some other state changes (if using state, endless loop)*/])
+  // // const [state, setState] = useState({ //have prop drilling
+  // //   someProp:'SomeVal'
+  // // })
+  // const [state, setState] = useState();
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Container>
+          <Nav variant = "tabs" defaultActiveKey = "/about">
+            <Nav.Item > <Nav.Link href = "/" >About</Nav.Link>    </Nav.Item>
+            <Nav.Item > <Nav.Link href = "/contact">Contact</Nav.Link> </Nav.Item>
+            <Nav.Item > <Nav.Link href = "/portfolio">Portfolio</Nav.Link> </Nav.Item>
+          </Nav>
+        </Container>
+        <Route exact path ="/" component =  {About} />
+        <Route path = "/Contact" component = {Contact}/>
+        <Route path ="/Portfolio"component = {Portfolio}/>
+      </Router>
     </div>
   );
 }
