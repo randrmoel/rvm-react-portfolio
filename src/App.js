8a9c4a77e9821ca12de395bf32ba995104fb6491
Robert Moel <robert.moel@rvmconsulting1.com>
2020-05-23 19:16:49 -0400

    add Contact Page

--- src/App.js
+++ src/App.js
@@ -8,18 +8,8 @@ import Footer from './components/Footer'
 import {Nav, Container} from 'react-bootstrap'
 import BackPic from './images/spikes.png'
 
-
-
 function App() {
 
-  // useEffect(()=>{
-
-  // },
-  // [/* some other state changes (if using state, endless loop)*/])
-  // // const [state, setState] = useState({ //have prop drilling
-  // //   someProp:'SomeVal'
-  // // })
-  // const [state, setState] = useState();
 
   const BackPicStyle =  {backgroundImage: `url(${BackPic})`, margin:"0%", padding:"1% 20% 15% 3%", width:"max-width"}
 
@@ -28,7 +18,7 @@ function App() {
       <Router>
         <Container>
           <Nav fill variant = "tabs" defaultActiveKey = "/about">
-            <Nav.Item style={{fontSize:"2em", color:"#4aaaa5"}}>Robert Moel</Nav.Item>
+            <Nav.Item style={{fontSize:"2em", color:"#ffffff", backgroundColor:"darkblue"}}>Robert Moel</Nav.Item>
             <Nav.Item > <Nav.Link href = "/" >About</Nav.Link></Nav.Item>
             <Nav.Item > <Nav.Link href = "/contact">Contact</Nav.Link> </Nav.Item>
             <Nav.Item > <Nav.Link href = "/portfolio">Portfolio</Nav.Link> </Nav.Item>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,38 @@
 import React from 'react'
-import { Container} from 'react-bootstrap'
+import { Container, Row, Col, Form, Button} from 'react-bootstrap'
 
 function Contact(props) {
     return(
         <div>
             <Container style = {props.BackPicStyle}>
-                Contact
+                <Row>
+                    <Col style={{marginLeft:"2%"}}>
+                        <h3>Contact Me</h3>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Form>
+                            <Form.Group controlId = "Name">
+                                <Form.Label>Name</Form.Label>
+                                <Form.Control type="input" placeholder="Enter your first and last name"/>
+                            </Form.Group>
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" />
+                                <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                                </Form.Text>
+                            </Form.Group>
+                            <Form.Group controlId="textArea">
+                                <Form.Label>Enter Comments Here</Form.Label>
+                                <Form.Control as="textarea" rows="5" />
+                            </Form.Group>
+                            <Button variant="primary" type="submit">Submit</Button>
+                        </Form>
+                    </Col>
+                </Row>
+
             </Container>
         </div>
     )
