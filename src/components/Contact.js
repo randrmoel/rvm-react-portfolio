9bd0f1bda6a82991791560f309e2ab0cb58151da
Robert Moel <robert.moel@rvmconsulting1.com>
2020-05-24 01:59:50 -0400

    make email client work

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,32 @@ import React from 'react'
 import { Container, Row, Col, Form, Button} from 'react-bootstrap'
 import '../styles/Contact.css'
 
+const initialFormData = Object.freeze({
+    name: "",
+    email: "",
+    inText: ""
+})
+
+
 function Contact(props) {
+    const [formData, updateFormData] = React.useState(initialFormData);
+
+    const handleChange = (e)=>{
+        updateFormData({
+            ...formData,
+            [e.target.name]:e.target.value.trim()
+        });
+    };
+
+    function sendEmail(e){
+        document.getElementById("formName").innerHTML="";
+        document.getElementById("formBasicEmail").innerHTML="";
+        document.getElementById("textArea").innerHTML="";
+        /*window.open(`"mailto:${formData.email}$subject=Contact Rob Moel from ${formData.name}&body=${formData.intext}"`)*/
+        console.log(formData.name, formData.email, formData.inText);
+        window.location.href = `mailto:robert.moel@rvmconsulting1.com?subject=Contact%20Rob%20Moel%20from%20${formData.email.split(" ").join("%20")}&body=${formData.inText.split(" ").join("%20")}`
+
+    }
     return(
         <div>
             <Container style = {props.BackPicStyle}>
@@ -15,26 +40,25 @@ function Contact(props) {
                 <Row>
                     <Col>
                         <Form>
-                            <Form.Group controlId = "Name">
+                            <Form.Group controlId = "formName">
                                 <Form.Label>Name</Form.Label>
-                                <Form.Control type="input" placeholder="Enter your first and last name"/>
+                                <Form.Control name="name" onChange = {handleChange} type="input" placeholder="Enter your first and last name"/>
                             </Form.Group>
                             <Form.Group controlId="formBasicEmail">
                                 <Form.Label>Email address</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" />
+                                <Form.Control name= "email" onChange = {handleChange} type="email" placeholder="Enter email" />
                                 <Form.Text className="text-muted">
                                 We'll never share your email with anyone else.
                                 </Form.Text>
                             </Form.Group>
                             <Form.Group controlId="textArea">
                                 <Form.Label>Enter Comments Here</Form.Label>
-                                <Form.Control as="textarea" rows="5" />
+                                <Form.Control name="inText" onChange={handleChange} as="textarea" rows="5" />
                             </Form.Group>
-                            <Button variant="primary" type="submit">Submit</Button>
+                            <Button onClick={sendEmail} variant="primary" type="submit">Submit</Button>
                         </Form>
                     </Col>
                 </Row>
-
             </Container>
         </div>
     )
